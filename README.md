4bef6a8bbecdb254e141776a81b9d557c66c3bc9
Sascha Ringström <sascha.ringstrom@redmind.se>
2019-01-15 19:26:01 +0100

    added trello stars

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ## Console++
-
 Adds styling related methods to console object.
 - `console.commands();` for brief showcase of most commands.
 - `console.help();` for more detailed information of the available commands.
@@ -7,7 +6,6 @@ Adds styling related methods to console object.
 #### [INSTALL CONSOLE++](https://github.com/Saschamz/userscripts/raw/master/Console%2B%2B.user.js)
 
 ## Bitbucker PR Template 
-
 Automatically fills the WYSIWYG editor in BitBucket when creating a new PR template.
 
 To change the customize the template make your changes in the editor and run the following
@@ -16,3 +14,9 @@ inside your browser terminal:
 localStorage.setItem('BITBUCKET_PR_TEMPLATE', document.querySelector('.ProseMirror').innerHTML)
 ```
 #### [INSTALL BITBUCKET PR TEMPLATE](https://github.com/Saschamz/userscripts/raw/master/bitbucket-pr-template.user.js)
+
+## Trello Stars
+Locally star trello cards you need to keep track of by hovering them and pressing `§`.
+
+Uses localstorage to keep track of your stored cards across browser sessions.
+#### [INSTALL TRELLO STARS](https://github.com/Saschamz/userscripts/raw/master/trello-stars.user.js)
--- trello-stars.user.js
+++ trello-stars.user.js
@@ -1,28 +1,46 @@
 // ==UserScript==
-// @name         Console++
+// @name         Trello Stars
 // @namespace    Sascha
 // @version      0.1
-// @description  CSS functions for the console object.
+// @description  Locally star trello cards you need to keep track of.
 // @author       Sascha Ringström
 // @match        *://*/*
 // @grant        none
-// @downloadURL  https://github.com/Saschamz/userscripts/raw/master/Console%2B%2B.user.js
+// @downloadURL  https://github.com/Saschamz/userscripts/raw/master/trello-stars.user.js
 // ==/UserScript==
 
 (function() {
-  const starredCards = JSON.parse(localStorage.getItem('trello-stars')) || {}
+  const starredCards = new Set(JSON.parse(localStorage.getItem('TRELLO_STARS'))) || {}
   const allVisibleCards = Array.from(document.querySelectorAll('.list-card'))
+  let hoveredCard = null
   
   allVisibleCards
-    .filter(card => starredCards[card.href])
+    .filter(card => starredCards.has(card.href))
     .forEach(card => card.style.background = 'gold')
 
   document.addEventListener('keydown', ({ key }) => {
-    if (/s/i.test(key)) {
-      console.log('add star')
+    if (key === '§' && hoveredCard) {
+      if (starredCards.has(hoveredCard.href)) {
+        starredCards.delete(hoveredCard.href)
+        hoveredCard.style.background = 'white'
+      } else {
+        starredCards.add(hoveredCard.href)
+        hoveredCard.style.background = 'gold'
+      }
+
+      localStorage.setItem('TRELLO_STARS', JSON.stringify(Array.from(starredCards)))
     }
   })
 
+  allVisibleCards.forEach(card => {
+    card.addEventListener('mouseenter', ({ target }) => {
+      hoveredCard = target
+    })
+    
+    card.addEventListener('mouseleave', () => {
+      hoveredCard = null
+    })
+  })
 })();
 
 
