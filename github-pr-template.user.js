49cec3023ec0e53ac53166b993ce0122cf6f00cf
Sascha Ringström <sascha.ringstrom@redmind.se>
2020-01-14 16:26:59 +0100

    fixed @match

--- github-pr-template.user.js
+++ github-pr-template.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Github PR Template
 // @author       Sascha Ringström
-// @match        https://github.com/*/**/compare/**/*
+// @match        https://github.com/*/**/compare/*
 // @grant        none
 // @downloadURL  https://github.com/Saschamz/userscripts/raw/master/github-pr-template.user.js
 // ==/UserScript==
@@ -27,7 +27,6 @@ function retryIfNoElement(
 }
 
 function main() {
-  console.log('executing main')
   retryIfNoElement(
     'textarea[id^=pull_request]',
     el => (el.value = PR_TEMPLATE_TEXT),
@@ -36,35 +35,36 @@ function main() {
   )
 }
 
-const PR_TEMPLATE_TEXT = `[ch]
-
-## Description
+const PR_TEMPLATE_TEXT = `## Description
 
 Description of your changes
 
 ## Checklist
 - [ ] At the top of this comment add the clubhouse story number e.g. \`[ch123]\`
-- [ ] Does the branch adhere to our branch naming conventions?
+- [ ] Does the branch adhere to our branch naming conventions [Document](document.com)
 - [ ] Breaking change (fix or feature that would cause existing functionality to change)
-- [ ] My change requires an update to the documentation.
+- [ ] My change requires a change to the documentation.
 - [ ] I have updated the documentation accordingly.
 - [ ] I have added tests to cover my changes.
 - [ ] Have you successfully ran tests with your changes locally?
 - [ ] All new and existing tests passed.
 - [ ] Have you added an explanation of what your changes do?
+- [ ] If you have added new environment variables, have you updated the .env.example
 
 ## Environment & Setup
+- [ ] :computer: node version => \`nvm use 10.13.0\`
 - [ ] :books: new libraries => \`npm ci\`
 - [ ] :iphone: native changes => \`pod install\`
+- [ ] :scroll: env file changes
 
 ## How to test/verify this feature
 
-Explain how in an ordered list of whats required to test this feature. 
+Explain how in an ordered list of whats required to test this feature.
 
-1. 
-2. 
-3. 
-4. 
+1.
+2.
+3.
+4.
 `
 
 ;(function() {
